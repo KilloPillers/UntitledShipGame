0edf98ae6e2e41c385c4b453e70e40b3ff70d24c
Siperoni <richardzhangdvs@gmail.com>
2024-12-09 12:59:53 -0800

    Added cutscene and menu background assets and implemented the cutscene

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,7 +1,21 @@
 extends Control
 
+@onready var video_player = $VideoStreamPlayer
+@onready var play_button = $MarginContainer/VBoxContainer/Play
+@onready var exit_button = $MarginContainer/VBoxContainer/Exit
+@onready var title_text = $Label
+
 func _on_exit_pressed() -> void:
 	pass
 
 func _on_play_pressed() -> void:
+	play_button.hide()
+	exit_button.hide()
+	title_text.hide()
+	video_player.show()
+	video_player.stream = preload("res://assets/FinalProjectCutsceneReducedFlash.ogv")
+	video_player.play()
+
+
+func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_file("res://scenes/map.tscn")
